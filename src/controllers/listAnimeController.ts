a7a42c2e7f441cc7598de9c1261329d6783a2788
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2022-03-16 19:30:41 +0700

    clear

--- src/controllers/listAnimeController.ts
+++
@@ -1,7 +0,0 @@
-// export default class listAnimeController {
-//   constructor(private readonly animeRepository: AnimeRepository) {}
-
-//   public async listAnime(): Promise<Anime[]> {
-//     return this.animeRepository.findAll();
-//   }
-// }
--- src/services/listAnimeService.ts
+++
@@ -1,7 +0,0 @@
-// export default class ListAnimeService {
-//   constructor(private readonly animeRepository: AnimeRepository) {}
-
-//   public async listAnime(): Promise<Anime[]> {
-//     return this.animeRepository.findAll();
-//   }
-// }
